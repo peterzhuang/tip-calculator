6bfb547007060de16fc1b54ffdbe06eac38445b2
Yuchuan <yuchuan@example.com>
2022-11-23 15:56:38 -0500

    refactor with immer in basic form

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import { ITEM_ADDED, ITEM_PRICE_UPDATED, ITEM_QUANTITY_UPDATED, ITEM_REMOVED } from './actions';
 
 let id = 1;
@@ -8,30 +9,51 @@ export const initialItems = [
 ];
 
 export const reducer = (state = initialItems, action) => {
+  // if(action.type === ITEM_ADDED) {
+  //   const item = { uuid: id++, quantity: 1, ...action.payload };
+  //   return [...state, item];
+  // }
+
   if(action.type === ITEM_ADDED) {
-    const item = { uuid: id++, quantity: 1, ...action.payload };
-    return [...state, item];
+    return produce(state, (draftState) => {
+      const item = { uuid: id++, quantity: 1, ...action.payload };
+      draftState.push(item);
+    });
   }
 
   if(action.type === ITEM_REMOVED) {
     return state.filter(item => item.uuid !== action.payload.uuid);
   }
 
+  // if(action.type === ITEM_PRICE_UPDATED) {
+  //   return state.map(item => {
+  //     if (item.uuid === action.payload.uuid) {
+  //       return { ...item, price: action.payload.price };
+  //     }
+  //     return item;
+  //   });
+  // }
+
   if(action.type === ITEM_PRICE_UPDATED) {
-    return state.map(item => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, price: action.payload.price };
-      }
-      return item;
+    return produce(state, (draftState) => {
+      const item = draftState.find(item => item.uuid === action.payload.uuid);
+      item.price = parseInt(action.payload.price, 10);
     });
   }
 
+  // if(action.type === ITEM_QUANTITY_UPDATED) {
+  //   return state.map(item => {
+  //     if (item.uuid === action.payload.uuid) {
+  //       return { ...item, quantity: action.payload.quantity };
+  //     }
+  //     return item;
+  //   });
+  // }
+
   if(action.type === ITEM_QUANTITY_UPDATED) {
-    return state.map(item => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, quantity: action.payload.quantity };
-      }
-      return item;
+    return produce(state, (draftState) => {
+      const item = draftState.find(item => item.uuid === action.payload.uuid);
+      item.quantity = parseInt(action.payload.quantity, 10);
     });
   }
 
